752212db42afafdee1b54d60fef944a1867665cd
Cristopher Namchee <cristophernamchee12@gmail.com>
2023-06-29 22:02:01 +0700

    feat: add logic for non-fuzzy directives

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,66 @@
 import core from '@actions/core';
 import github from '@actions/github';
 
+import { Configuration, OpenAIApi } from 'openai';
+
+interface Knowledge {
+  id: number;
+  title: string;
+  summary: string;
+  solution: string;
+}
+
+async function callGPT(token: string) {
+  const configuration = new Configuration({
+    apiKey: token,
+  });
+
+  const client = new OpenAIApi(configuration);
+
+  const completion = await client.createChatCompletion();
+}
+
+async function saveKnowledge(knowledge: Knowledge) {
+
+}
+
 async function run(): Promise<void> {
   try {
     const accessToken = core.getInput('access_token');
     const openAIKey = core.getInput('openai_key');
 
     const octokit = github.getOctokit(accessToken);
-    const { owner, repo } = github.context.issue;
-    
+    const { number, owner, repo } = github.context.issue;
+
     const comments = await octokit.request("GET /repos/{owner}/{repo}/issues/comments", {
       owner,
       repo,
     });
     
-    const texts = comments.data.map(comment => comment.body);
-    const anchor = texts.find(text => text?.startsWith('/summarizr'));
+    const texts = (comments.data).map(comment => comment.body) as string[];
+    const anchor = texts.find(text => text === '/summarizr');
 
     if (!anchor) {
       return;
     }
 
+    const issue = await octokit.rest.issues.get({
+      issue_number: number,
+      owner,
+      repo,
+    });
+
     const anchorSummary = /[pP]roblems?:\n\n?([\s\S]+?)\n\n[sS]olutions?:\n\n?([\s\S]+)/ig.exec(anchor);
 
     if (anchorSummary) {
+      const [_, problem, solution] = anchorSummary;
 
+      await saveKnowledge({
+        id: number,
+        title: issue.data.title,
+        summary: problem,
+        solution: solution,
+      });
     } else {
       // Call GPT
     }

0df8505df363558a1fd6fe42ec6300bd549dfdc4
Cristopher Namchee <cristophernamchee12@gmail.com>
2023-06-29 21:42:21 +0700

    feat: add base problem-summary extraction

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,33 @@
-import * as core from '@actions/core';
+import core from '@actions/core';
+import github from '@actions/github';
 
 async function run(): Promise<void> {
   try {
+    const accessToken = core.getInput('access_token');
     const openAIKey = core.getInput('openai_key');
 
-    console.log(openAIKey);
+    const octokit = github.getOctokit(accessToken);
+    const { owner, repo } = github.context.issue;
+    
+    const comments = await octokit.request("GET /repos/{owner}/{repo}/issues/comments", {
+      owner,
+      repo,
+    });
+    
+    const texts = comments.data.map(comment => comment.body);
+    const anchor = texts.find(text => text?.startsWith('/summarizr'));
+
+    if (!anchor) {
+      return;
+    }
+
+    const anchorSummary = /[pP]roblems?:\n\n?([\s\S]+?)\n\n[sS]olutions?:\n\n?([\s\S]+)/ig.exec(anchor);
+
+    if (anchorSummary) {
+
+    } else {
+      // Call GPT
+    }
   } catch (err) {
     core.setFailed(err);
   }

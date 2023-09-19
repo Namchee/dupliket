27dce70b18dedd777fc78fba34d53bfc9f86db8a
Cristopher Namchee <cristophernamchee12@gmail.com>
2023-09-19 21:31:51 +0700

    chore: remove debug from markdown sanitazion

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -3,18 +3,10 @@ import { remark } from 'remark';
 import stripMarkdown from 'strip-markdown';
 import githubFlavoredMarkdown from 'remark-gfm';
 
-import { logDebug } from './logger';
-
 export function sanitizeMarkdown(text: string): string {
-  logDebug(`Before: ${text}`);
-
-  const result = remark()
+  return remark()
     .use(githubFlavoredMarkdown)
     .use(stripMarkdown, { keep: ['inlineCode', 'code'] })
     .processSync(text)
     .toString();
-
-  logDebug(`After: ${result}`);
-
-  return result;
 }
